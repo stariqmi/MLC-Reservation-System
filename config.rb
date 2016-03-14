0dc40d5550c6af7f985a5da20fb4895f07edafe7
Salman Tariq Mirza <salmantariq_mirza@apple.com>
2016-03-13 19:32:49 -0700

    production DB and source key

--- config.rb
+++ config.rb
@@ -1,28 +1,32 @@
 class ServerConfig
     
-    attr_reader :thinPort, :mongoHost, :mongoUrl, :mongoDB, :env, :usaEpayKey, :usaEpayPin
+    attr_reader :thinPort, :mongoUrl, :mongoDB, :env, :usaEpayKey, :usaEpayPin
     
     # Initialize instance variables that define server configuration
     def initialize
         # Thin Server configuration
         @thinPort = 3458
         @env = :development
-        
-        # MongoDB configuration
-        @mongoHost = "localhost"
-        @mongoPort = "27017"
-
 
     	# Development DB
-    	@mongoDevUrl = "mongodb://root:root@ds023408.mlab.com:23408"
-    	@mongoDevDB = "mlcdevelopment"
-            
-    	# Use Dev DB
-    	@mongoUrl = @mongoDevUrl
-    	@mongoDB = @mongoDevDB
+    	mongoDevUrl = "mongodb://root:root@ds023408.mlab.com:23408"
+    	mongoDevDB = "mlcdevelopment"
         
-        @usaEpayKey = "_JYPW26imL8vZUFNU0wdnHqRlbBANZwK" # Development Key
-        # @usaEpayKey = "tqgAND730mEqFY22B7Tmf2mno7mvV7aR" # Production Key
+        # Production DB
+        mongoUrl = "mongodb://root:root@ds015209-a0.mlab.com:15209"
+        mongoDB = "mlcproduction"
+            
+    	# # Use Dev DB
+    	# @mongoUrl = mongoDevUrl
+    	# @mongoDB = mongoDevDB
+        
+        # Use Production DB
+        @mongoUrl = mongoUrl
+        @mongoDB = mongoDB
+    
+        
+        # @usaEpayKey = "_JYPW26imL8vZUFNU0wdnHqRlbBANZwK" # Development Key
+        @usaEpayKey = "tqgAND730mEqFY22B7Tmf2mno7mvV7aR" # Production Key
         @usaEpayPin = "9999"
     end
 end
